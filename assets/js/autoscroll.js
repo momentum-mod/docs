3075fcde1bc7c6b12b6568ebf823ccc71249c2fd
brae <brae@braebie.com>
2020-10-17 01:53:51 -0700

    - jquery references in autoscroll script
    Site does not include jquery causing this to break

--- assets/js/autoscroll.js
+++ assets/js/autoscroll.js
@@ -1,6 +1,6 @@
-const scrollElem = SimpleBar.instances.get($('.sidebar')[0]).getScrollElement();
+const scrollElem = SimpleBar.instances.get(document.getElementsByClassName('sidebar')[0]).getScrollElement();
 scrollElem.scrollBy({
-  top: $('#sidebar-anchor').position().top - Math.max(scrollElem.clientHeight) / 2 + $('.masthead')[0].clientHeight,
+  top: document.getElementById('sidebar-anchor').offsetTop - Math.max(scrollElem.clientHeight) / 2 + document.getElementsByClassName('masthead')[0].clientHeight,
   left: 0,
   behavior: 'smooth'
 });
